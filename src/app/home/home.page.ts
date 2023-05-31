c286c36401626db82cc59305333f01dd43485bbd
d.dieleuterio <d>
2023-05-31 17:35:58 +0200

    push strano

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,9 +11,64 @@
 
 // }
 
+// import { Component, OnInit, ViewChild } from '@angular/core';
+// import { DragScrollComponent } from 'ngx-drag-scroll';
+// import { ComuniService } from '../services/comuni.service';
+
+// interface Comune {
+//   Sigla: string;
+//   Provincia: string;
+//   Superficie: number;
+//   Residenti: number;
+//   NumComuni: number;
+//   IdRegione: number;
+//   CodIstat: string;
+//   Image: string;
+// }
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: 'home.page.html',
+//   styleUrls: ['home.page.scss'],
+// })
+// export class HomePage implements OnInit {
+//   @ViewChild('nav', { read: DragScrollComponent }) ds!: DragScrollComponent;
+
+//   items: Comune[] = [];
+//   page = 1;
+//   perPage = 20;
+
+//   constructor(private comuniService: ComuniService) {}
+
+//   ngOnInit() {
+//     this.loadComuni();
+//   }
+
+//   loadComuni() {
+//     this.comuniService.getComuni(this.page, this.perPage).subscribe(res => {
+//       console.log('Risposta dal server:', res);
+//       this.items.push(...res);
+//       this.page++;
+//     });
+//   }
+
+//   onScroll(event: any) {
+//     const scrollTop = event.target.scrollTop;
+//     const scrollHeight = event.target.scrollHeight;
+//     const offsetHeight = event.target.offsetHeight;
+//     const isEndOfScroll = (scrollTop + offsetHeight) >= scrollHeight;
+
+//     if (isEndOfScroll) {
+//       this.loadComuni();
+//     }
+//   }
+// }
+
+
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DragScrollComponent } from 'ngx-drag-scroll';
 import { ComuniService } from '../services/comuni.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 interface Comune {
   Sigla: string;
@@ -38,17 +93,22 @@ export class HomePage implements OnInit {
   page = 1;
   perPage = 20;
 
-  constructor(private comuniService: ComuniService) {}
+  constructor(private comuniService: ComuniService, private domSanitizer: DomSanitizer) {}
 
   ngOnInit() {
     this.loadComuni();
   }
 
   loadComuni() {
-    this.comuniService.getComuni(this.page, this.perPage).subscribe(res => {
+    this.comuniService.getComuni(this.page, this.perPage).subscribe((res: Comune[]) => {
+      console.log('Risposta dal server:', res);
       this.items.push(...res);
       this.page++;
     });
+  }
+
+  sanitizeImage(image: string): SafeUrl {
+    return this.domSanitizer.bypassSecurityTrustUrl(image);
   }
 
   onScroll(event: any) {
@@ -62,5 +122,4 @@ export class HomePage implements OnInit {
     }
   }
 }
-
 
